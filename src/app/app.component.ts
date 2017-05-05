e4065db0be00a666a0bdc2b0e5d7357a24011245
Lucas Colomer <lco@frianbiz.com>
2017-05-05 11:21:46 +0200

    feat(Page): create and add all pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { BuzzerComponent } from './buzzer/buzzer.component';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  public isDisplayBuzzer: Boolean
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
+import { GameMasterComponent } from './game-master/game-master.component';
+import { ClientComponent } from './client/client.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BuzzerComponent } from './buzzer/buzzer.component';
+
+import { Routes, RouterModule } from '@angular/router';
+import { QuestionsComponent } from './questions/questions.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuzzerComponent,
+    GameMasterComponent,
+    ClientComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'gamemaster',
+        component: GameMasterComponent
+      },
+      {
+        path: 'client',
+        component: ClientComponent
+      },
+      {
+        path: 'questions',
+        component: QuestionsComponent
+      },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
