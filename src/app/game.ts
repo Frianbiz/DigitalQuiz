75945b00ecfb739184df7b62dcce3519b49c03f1
Lucas Colomer <lco@frianbiz.com>
2017-05-05 15:21:54 +0200

    feat(State):

--- src/app/game.ts
+++ src/app/game.ts
@@ -1,9 +1,11 @@
 export class Game {
     public state: String;
     public activeUser: String;
+    public question: number;
 
-    constructor(state: String, activeUser: String) {
+    constructor(state: String, activeUser: String, question: number) {
         this.state = state;
         this.activeUser = activeUser;
+        this.question = question;
     }
 }
